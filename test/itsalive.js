cc4939e1d9d3221dd87066d3c9d898ee559d391c
Sarah <sarah@Garretts-MacBook-Pro.local>
2018-01-23 12:45:07 -0500

    spy complete

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,4 +1,7 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
+const spies = require('chai-spies');
+const chai = require('chai');
+chai.use(spies);
 
 describe('simple test adder', function() {
   it('adds 2 and 2 and returns 4', function() {
@@ -12,10 +15,24 @@ describe('timeOut tester', function() {
       var start = new Date();
     setTimeout(function() {
       var duration = new Date() - start;
+      expect(duration).to.be.closeTo(1000, 50);
       done()
     }, 1000)
-      expect(duration).to.be.closeTo(1000, 50)
+
   });
+})
+
+
+describe('spying on stuff', function() {
+  it('spies on a forEach function', function() {
+    var array = [1, 2, 3, 4, 5]
+    function logNum(val) {
+      console.log(val);
+    }
+    logNum = chai.spy(logNum)
+    array.forEach(logNum);
+    expect(logNum).to.have.been.called.exactly(array.length);
+  })
 })
 
 
